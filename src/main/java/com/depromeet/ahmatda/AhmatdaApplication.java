6df8f1d4ac6d1529cd793d92668dff9ff93fb10f
sungmin69355 <sungmin69355@naver.com>
2022-10-06 22:38:20 +0900

    초기 프로젝트 setting

---
+++ src/main/java/com/depromeet/ahmatda/AhmatdaApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.ahmatda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhmatdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhmatdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depromeet/ahmatda/AhmatdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.ahmatda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhmatdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
