9c32359045c7e4fa19cd0e80d9a9a552d9b86f96
자신의 닉네임 <cmg1411@naver.com>
2022-10-24 10:10:05 +0900

    :fire: custom snippet 클래스 정의

---
+++ src/test/java/com/depromeet/ahmatda/apidocs/snippet/CustomRequestHeaderSnippet.java
@@ -0,0 +1,26 @@
+package com.depromeet.ahmatda.apidocs.snippet;
+
+import org.springframework.restdocs.headers.AbstractHeadersSnippet;
+import org.springframework.restdocs.headers.HeaderDescriptor;
+import org.springframework.restdocs.operation.Operation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class CustomRequestHeaderSnippet extends AbstractHeadersSnippet {
+
+    public static CustomRequestHeaderSnippet customRequestHeaders(String type, Map<String, Object> attributes, HeaderDescriptor... descriptors) {
+        return new CustomRequestHeaderSnippet(type, Arrays.asList(descriptors), attributes);
+    }
+
+    private CustomRequestHeaderSnippet(String type, List<HeaderDescriptor> descriptors, Map<String, Object> attributes) {
+        super(type, descriptors, attributes);
+    }
+
+    @Override
+    protected Set<String> extractActualHeaders(Operation operation) {
+        return operation.getRequest().getHeaders().keySet();
+    }
+}
