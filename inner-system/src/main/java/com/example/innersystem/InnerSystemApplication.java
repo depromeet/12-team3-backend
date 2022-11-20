43f229eac2ec049e284f2b9de206d356b2fa29db
자신의 닉네임 <cmg1411@naver.com>
2022-11-20 21:12:30 +0900

    :wrench: 모듈 추가

---
+++ inner-system/src/main/java/com/example/innersystem/InnerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.innersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnerSystemApplication.class, args);
+    }
+
+}
