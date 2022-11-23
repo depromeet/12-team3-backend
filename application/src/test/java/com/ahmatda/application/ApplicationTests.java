d00b8479181d652f1341536750e6e441bc9f2c86
Cho Sungmin <sungmin69355@naver.com>
2022-11-23 22:48:24 +0900

    :rocket: SpringBootTest classes 지정

--- application/src/test/java/com/ahmatda/application/ApplicationTests.java
+++ application/src/test/java/com/ahmatda/application/ApplicationTests.java
@@ -3,7 +3,7 @@ package com.ahmatda.application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 class ApplicationTests {
 
     @Test
