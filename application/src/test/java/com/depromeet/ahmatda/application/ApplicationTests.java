7071710970c4afa32e70b1a24fa90fe292de7fa5
자신의 닉네임 <cmg1411@naver.com>
2022-11-26 17:42:51 +0900

    test 용 profile, properties 추가

--- application/src/test/java/com/depromeet/ahmatda/application/ApplicationTests.java
+++ application/src/test/java/com/depromeet/ahmatda/application/ApplicationTests.java
@@ -3,7 +3,9 @@ package com.depromeet.ahmatda.application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.depromeet.ahmatda.Application;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(classes = Application.class)
 class ApplicationTests {
 
