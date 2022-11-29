c4f4b993ff66250db8c22ac4f3e0e73de82c5c9c
자신의 닉네임 <cmg1411@naver.com>
2022-11-30 03:51:00 +0900

    :fire: 회원가입에 DeviceCode 추가

--- application/src/main/java/com/depromeet/ahmatda/user/UserControllerAdvice.java
+++ application/src/main/java/com/depromeet/ahmatda/user/UserControllerAdvice.java
@@ -1,16 +1,23 @@
 package com.depromeet.ahmatda.user;
 
+import com.depromeet.ahmatda.common.response.ErrorCode;
 import com.depromeet.ahmatda.common.response.RestResponse;
 import com.depromeet.ahmatda.user.exception.UserExistException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice(basePackageClasses = { UserController.class })
 public class UserControllerAdvice {
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<RestResponse<Object>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body(RestResponse.error(ErrorCode.BINDING_ERROR));
+    }
 
     @ExceptionHandler(value = UserExistException.class)
     public ResponseEntity<RestResponse<Object>> handleUserExistException(UserExistException e) {
