4445bff1bdeb23c519a691c9509574ac39bb8d56
suchan <aud248@naver.com>
2022-11-29 16:33:18 +0900

    :white_check_mark: getCategories 컨트롤러 테스트 추가

---
+++ application/src/test/java/com/depromeet/ahmatda/category/CategoryControllerTest.java
@@ -0,0 +1,43 @@
+package com.depromeet.ahmatda.category;
+
+import com.depromeet.ahmatda.domain.user.adaptor.UserAdaptor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static com.depromeet.ahmatda.application.apidocs.util.ApiDocsUtil.getDocumentRequest;
+import static com.depromeet.ahmatda.application.apidocs.util.ApiDocsUtil.getDocumentResponse;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@SpringBootTest
+class CategoryControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @DisplayName("GET: /api/category 요청 시 모든 카테고리를 반환한다")
+    @Test
+    void getCategories() throws Exception {
+        mockMvc.perform(get("/api/category"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(document("category 열기",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        responseFields(
+                                fieldWithPath("result").type(JsonFieldType.ARRAY).description("카테고리"),
+                                fieldWithPath("error").type(JsonFieldType.NULL).description("에러"))));
+    }
+}
