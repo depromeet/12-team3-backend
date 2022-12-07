eeceacab3068bce214f62dd5fec7099024020f91
자신의 닉네임 <cmg1411@naver.com>
2022-12-08 01:11:01 +0900

    :fire: 기존 회원가입 로직 삭제, UserToken 으로 회원 여부 체크기능 추가

--- application/src/main/java/com/depromeet/ahmatda/user/service/impl/DeviceUserService.java
+++ application/src/main/java/com/depromeet/ahmatda/user/service/impl/DeviceUserService.java
@@ -1,12 +1,9 @@
 package com.depromeet.ahmatda.user.service.impl;
 
-import com.depromeet.ahmatda.common.response.ErrorCode;
-import com.depromeet.ahmatda.domain.user.User;
 import com.depromeet.ahmatda.domain.user.adaptor.UserAdaptor;
-import com.depromeet.ahmatda.user.exception.UserExistException;
-import com.depromeet.ahmatda.user.dto.SignUpRequestDto;
-import com.depromeet.ahmatda.user.exception.UserNotExistException;
+import com.depromeet.ahmatda.user.UserRegisterCode;
 import com.depromeet.ahmatda.user.service.UserService;
+import com.depromeet.ahmatda.user.token.UserToken;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,24 +16,11 @@ public class DeviceUserService implements UserService {
     }
 
     @Override
-    public User createUser(SignUpRequestDto request) {
-        String deviceId = request.getDeviceId();
-        checkExistUser(deviceId);
+    public UserRegisterCode checkUser(UserToken userToken) {
+        String userTokenValue = userToken.getValue();
 
-        User user = User.createUserWithDeviceId(request.getDeviceCode(), request.getDeviceId());
-        return userAdaptor.createUser(user);
-    }
-
-    private void checkExistUser(String deviceId) {
-        userAdaptor.getByDeviceId(deviceId).ifPresent(t -> {
-            throw new UserExistException(ErrorCode.EXIST_USER);
-        });
-    }
-
-    @Override
-    public User getByDeviceId(String deviceId) {
-        return userAdaptor.getByDeviceId(deviceId).orElseThrow(() ->
-                new UserNotExistException("유저가 존재하지 않습니다")
+        return UserRegisterCode.getByUserFound(
+                userAdaptor.findByUserToken(userTokenValue).isPresent()
         );
     }
 }
