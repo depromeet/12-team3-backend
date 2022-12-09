5aca82dbd43b644220e454f90e0ce6cf63bc1100
자신의 닉네임 <cmg1411@naver.com>
2022-12-10 02:39:45 +0900

    serializer 삭제

--- application/src/main/java/com/depromeet/ahmatda/user/token/UserToken.java
+++ application/src/main/java/com/depromeet/ahmatda/user/token/UserToken.java
@@ -1,14 +1,10 @@
 package com.depromeet.ahmatda.user.token;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.UUID;
 
-@JsonSerialize(using = UserTokenSerializer.class)
-@JsonDeserialize(using = UserTokenDeSerializer.class)
 @Getter
 @AllArgsConstructor
 public class UserToken {
