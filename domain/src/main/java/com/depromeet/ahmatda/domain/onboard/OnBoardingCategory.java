05022f4c2d40e0ea2123a8b08f138545cf01577f
자신의 닉네임 <cmg1411@naver.com>
2022-12-12 00:37:06 +0900

    :fire: 온보딩 고정 카테고리 enum

---
+++ domain/src/main/java/com/depromeet/ahmatda/domain/onboard/OnBoardingCategory.java
@@ -0,0 +1,17 @@
+package com.depromeet.ahmatda.domain.onboard;
+
+import com.depromeet.ahmatda.domain.emoji.AhmatdaEmoji;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum OnBoardingCategory {
+
+    DAILY(AhmatdaEmoji.WORK, "일상"),
+    WORKING(AhmatdaEmoji.GYM, "운동"),
+    TRAVEL(AhmatdaEmoji.PLANE, "여행");
+
+    private AhmatdaEmoji emoji;
+    private String categoryName;
+}
