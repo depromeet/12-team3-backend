9b68dfc2b331972c24fe84c966b01aea628b11b9
자신의 닉네임 <cmg1411@naver.com>
2022-12-12 00:37:26 +0900

    :fire: 온보딩 및 회원가입

--- application/src/main/java/com/depromeet/ahmatda/user/dto/SignUpOnBoardRequest.java
+++ application/src/main/java/com/depromeet/ahmatda/user/dto/SignUpOnBoardRequest.java
@@ -1,15 +1,17 @@
 package com.depromeet.ahmatda.user.dto;
 
+import com.depromeet.ahmatda.domain.onboard.OnBoardingCategory;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 public class SignUpOnBoardRequest {
 
-    @NotBlank
-    private String category;
+    @NotNull
+    OnBoardingCategory category;
     @NotBlank
     private String templateName;
     private List<String> items;
--- application/src/main/java/com/depromeet/ahmatda/user/service/impl/DeviceUserService.java
+++ application/src/main/java/com/depromeet/ahmatda/user/service/impl/DeviceUserService.java
@@ -1,8 +1,14 @@
 package com.depromeet.ahmatda.user.service.impl;
 
+import com.depromeet.ahmatda.category.dto.CategoryRequest;
 import com.depromeet.ahmatda.category.service.CategoryService;
+import com.depromeet.ahmatda.domain.category.Category;
+import com.depromeet.ahmatda.domain.onboard.OnBoardingCategory;
 import com.depromeet.ahmatda.domain.user.User;
 import com.depromeet.ahmatda.domain.user.adaptor.UserAdaptor;
+import com.depromeet.ahmatda.template.dto.CreateTemplateRequest;
+import com.depromeet.ahmatda.template.dto.TemplateItemRequest;
+import com.depromeet.ahmatda.template.service.TemplateService;
 import com.depromeet.ahmatda.user.UserRegisterCode;
 import com.depromeet.ahmatda.user.dto.SignUpOnBoardRequest;
 import com.depromeet.ahmatda.user.service.UserService;
@@ -11,6 +17,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +26,7 @@ public class DeviceUserService implements UserService {
 
     private final UserAdaptor userAdaptor;
     private final CategoryService categoryService;
+    private final TemplateService templateService;
 
     @Override
     public UserRegisterCode checkUser(UserToken userToken) {
@@ -32,9 +41,45 @@ public class DeviceUserService implements UserService {
     @Transactional
     public UserToken createUser(SignUpOnBoardRequest request) {
         UserToken userToken = UserToken.generate();
-        User user = User.createUserWithUserToken(userToken.getValue());
-        userAdaptor.createUser(user);
+
+        User user = userAdaptor.createUser(
+            User.createUserWithUserToken(userToken.getValue())
+        );
+
+        long userId = user.getId();
+        Category category = createOnboardingCategory(userId, request.getCategory());
+        createOnboardingTemplate(userId, request.getTemplateName(), category, request.getItems());
 
         return new UserToken(user.getUserToken());
+    }
+
+    private Category createOnboardingCategory(Long userId, OnBoardingCategory category) {
+        CategoryRequest categoryRequest = CategoryRequest.builder()
+            .name(category.getCategoryName())
+            .type(category.getCategoryName())
+            .emoji(category.getEmoji())
+            .build();
+
+        return categoryService.createCategory(userId.toString(), categoryRequest);
+    }
+
+    private void createOnboardingTemplate(Long userId, String templateName, Category category, List<String> items) {
+        long categoryId = category.getId();
+
+        List<TemplateItemRequest> itemsRequests = items.stream()
+            .map(itemName ->
+                TemplateItemRequest.builder()
+                    .categoryId(categoryId)
+                    .name(itemName)
+                    .build()
+            ).collect(Collectors.toList());
+
+        CreateTemplateRequest templateRequest = CreateTemplateRequest.builder()
+                .templateName(templateName)
+                .categoryId(category.getId())
+                .items(itemsRequests)
+                .build();
+
+        templateService.createUserTemplate(userId.toString(), templateRequest);
     }
 }
