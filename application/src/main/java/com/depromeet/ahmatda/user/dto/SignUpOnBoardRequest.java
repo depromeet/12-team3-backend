2f6b94bba9a6bc4142bb73ecf77743a2cf55bf54
자신의 닉네임 <cmg1411@naver.com>
2022-12-12 23:57:27 +0900

    :fire: SignUpOnBoardRequest 생성자 추가

--- application/src/main/java/com/depromeet/ahmatda/user/dto/SignUpOnBoardRequest.java
+++ application/src/main/java/com/depromeet/ahmatda/user/dto/SignUpOnBoardRequest.java
@@ -1,9 +1,15 @@
 package com.depromeet.ahmatda.user.dto;
 
 import com.depromeet.ahmatda.onboard.OnboardingRequest;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpOnBoardRequest {
 
     private OnboardingRequest onboardingRequest;
