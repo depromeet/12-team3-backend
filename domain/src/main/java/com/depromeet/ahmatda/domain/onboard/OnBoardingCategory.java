3c8682d0add54782912d55c8e5c3b3b56ef43912
자신의 닉네임 <cmg1411@naver.com>
2022-12-13 01:35:56 +0900

    :fire: EnumType 상속

--- domain/src/main/java/com/depromeet/ahmatda/domain/onboard/OnBoardingCategory.java
+++ domain/src/main/java/com/depromeet/ahmatda/domain/onboard/OnBoardingCategory.java
@@ -1,12 +1,13 @@
 package com.depromeet.ahmatda.domain.onboard;
 
+import com.depromeet.ahmatda.common.utils.EnumType;
 import com.depromeet.ahmatda.domain.emoji.AhmatdaEmoji;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum OnBoardingCategory {
+public enum OnBoardingCategory implements EnumType {
 
     DAILY(AhmatdaEmoji.WORK, "일상"),
     EXERCISE(AhmatdaEmoji.GYM, "운동"),
@@ -14,4 +15,14 @@ public enum OnBoardingCategory {
 
     private AhmatdaEmoji emoji;
     private String categoryName;
+
+    @Override
+    public String getName() {
+        return this.name();
+    }
+
+    @Override
+    public String getLabel() {
+        return this.categoryName;
+    }
 }
