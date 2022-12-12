aea912ae598326d8f7faa4e7d2180d4b5c850a47
자신의 닉네임 <cmg1411@naver.com>
2022-12-12 23:56:57 +0900

    :fire: OnboardingRequest 생성자 추가

--- application/src/main/java/com/depromeet/ahmatda/onboard/OnboardingRequest.java
+++ application/src/main/java/com/depromeet/ahmatda/onboard/OnboardingRequest.java
@@ -1,13 +1,19 @@
 package com.depromeet.ahmatda.onboard;
 
 import com.depromeet.ahmatda.domain.onboard.OnBoardingCategory;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class OnboardingRequest {
 
     @NotNull
