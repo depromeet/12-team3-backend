1849211da63f519bc8e02589a35e76966a63a494
suchan <aud248@naver.com>
2022-12-15 13:23:09 +0900

    :fire: CorsFilter -> WebMvcConfigurer

--- application/src/main/java/com/depromeet/ahmatda/filter/CorsFilter.java
+++ application/src/main/java/com/depromeet/ahmatda/filter/CorsFilter.java
@@ -1,31 +1,16 @@
 package com.depromeet.ahmatda.filter;
 
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-    }
+@Configuration
+public class CorsFilter implements WebMvcConfigurer {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "https://*.12-team3-web.pages.dev");
-        httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
-    }
-
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://*.12-team3-web.pages.dev")
+                .allowedMethods("*");
     }
 }
