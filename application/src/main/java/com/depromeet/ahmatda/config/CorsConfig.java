87b3784326ba8f456a2b70c7c48929ce91708a50
hyesung oh <haesungoh414@gmail.com>
2022-12-16 08:25:24 +0900

    CORS mapping to all origin (#28)
    * hotfix: cors mapping to all origin
    
    * Update CorsConfig
    
    Co-authored-by: SuchanMyoung <aud248@naver.com>

--- application/src/main/java/com/depromeet/ahmatda/config/CorsConfig.java
+++ application/src/main/java/com/depromeet/ahmatda/config/CorsConfig.java
@@ -1,8 +1,6 @@
 package com.depromeet.ahmatda.config;
 
-import com.depromeet.ahmatda.HttpHeader;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,12 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://*.12-team3-web.pages.dev")
-                .allowedOrigins("https://12-team3-web.pages.dev")
-                .allowedHeaders(HttpHeader.USER_TOKEN)
-                .allowedMethods(HttpMethod.GET.name())
-                .allowedMethods(HttpMethod.POST.name())
-                .allowedMethods(HttpMethod.DELETE.name())
-                .allowedMethods(HttpMethod.PATCH.name());
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "DELETE", "PATCH", "OPTIONS", "HEAD");
     }
 }
