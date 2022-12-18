3f2ac3986c9c296e03635e35fa6f82bcbae39294
sungmin69355 <sungmin69355@naver.com>
2022-12-18 14:08:35 +0900

    :white_check_mark: 추천소지품,카테고리 유저템플릿에 추가 테스트 작성

--- application/src/test/java/com/depromeet/ahmatda/application/apidocs/document/recommend/RecommendControllerTest.java
+++ application/src/test/java/com/depromeet/ahmatda/application/apidocs/document/recommend/RecommendControllerTest.java
@@ -2,17 +2,23 @@ package com.depromeet.ahmatda.application.apidocs.document.recommend;
 
 import com.depromeet.ahmatda.HttpHeader;
 import com.depromeet.ahmatda.application.apidocs.document.ApiDocumentationTest;
+import com.depromeet.ahmatda.category.dto.CategoryRequest;
 import com.depromeet.ahmatda.category.dto.CategoryResponse;
+import com.depromeet.ahmatda.common.response.RestResponse;
 import com.depromeet.ahmatda.domain.category.CategoryType;
 import com.depromeet.ahmatda.domain.emoji.AhmatdaEmoji;
 import com.depromeet.ahmatda.domain.user.User;
-import com.depromeet.ahmatda.recommend.dto.RecommendTemplateItemResponse;
-import com.depromeet.ahmatda.recommend.dto.RecommendTemplateResponse;
+import com.depromeet.ahmatda.recommend.dto.*;
+import com.depromeet.ahmatda.template.dto.TemplateAddItemsRequest;
+import com.depromeet.ahmatda.template.dto.TemplateItemRequest;
 import com.depromeet.ahmatda.template.dto.TemplateItemResponse;
 import com.depromeet.ahmatda.template.dto.TemplateResponse;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.depromeet.ahmatda.application.apidocs.util.ApiDocsUtil.getDocumentRequest;
@@ -23,9 +29,10 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.headerWit
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class RecommendControllerTest extends ApiDocumentationTest {
@@ -94,4 +101,123 @@ public class RecommendControllerTest extends ApiDocumentationTest {
                                 headerWithName("ahmatda-user-token").description("유저 UUID")
                         )));
     }
+
+    @Test
+    void 추천템플릿_새로운_유저카테고리_생성() throws Exception {
+        User user = User.createUserWithUserToken("FCDBD8EF-62FC-4ECB-B2F5-92C9E79AC7F0");
+        String userToken = user.getUserToken();
+        Long userCategoryId = 1L;
+        Long userTemplateId = 10L;
+        List<String> items = Lists.newArrayList("여권", "증명사진", "노트북");
+
+        // 새로운 카테고리 + 새로운 템플릿에 추가
+        CategoryRequest categoryRequest = CategoryRequest.builder()
+                .type(CategoryType.EXERCISE).emoji(AhmatdaEmoji.TUBE).name("CUSTOM")
+                .build();
+
+        CreateNotCategoryTemplateRequest createNotCategoryTemplateRequest = CreateNotCategoryTemplateRequest.builder()
+                .templateName("추천템플릿제목")
+                .items(items)
+                .build();
+
+        CreateAllRequest createAllRequest = CreateAllRequest.builder()
+                .categoryRequest(categoryRequest)
+                .createTemplateRequest(createNotCategoryTemplateRequest)
+                .build();
+
+        RecommendAddUserTemplateRequest recommendAddUserTemplateRequest = RecommendAddUserTemplateRequest.builder()
+                .createAllRequest(createAllRequest)
+                .build();
+
+        String request = objectMapper.writeValueAsString(recommendAddUserTemplateRequest);
+        String response = objectMapper.writeValueAsString(RestResponse.ok());
+
+        mockMvc.perform(post("/api/recommend")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request)
+                        .header(HttpHeader.USER_TOKEN, userToken))
+                .andExpect(status().isOk())
+                .andExpect(content().string(response))
+                .andDo(document("recommend-create-category",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestHeaders(
+                                headerWithName("ahmatda-user-token").description("유저 UUID")
+                        )))
+                .andDo(print());
+    }
+
+    @Test
+    void 추천템플릿_기존_카테고리에_소지품저장() throws Exception {
+        User user = User.createUserWithUserToken("FCDBD8EF-62FC-4ECB-B2F5-92C9E79AC7F0");
+        String userToken = user.getUserToken();
+        Long userCategoryId = 1L;
+        Long userTemplateId = 10L;
+        List<String> items = Lists.newArrayList("여권", "증명사진", "노트북");
+
+        // 기존카테고리 + 기존템플릿에 추가
+        TemplateAddItemsRequest templateAddItemsRequest = TemplateAddItemsRequest.builder()
+                .userTemplateId(userTemplateId)
+                .userCategoryId(userCategoryId)
+                .items(items)
+                .build();
+
+        RecommendAddUserTemplateRequest recommendAddUserTemplateRequest = RecommendAddUserTemplateRequest.builder()
+                .templateAddItemsRequest(templateAddItemsRequest)
+                .build();
+
+        String request = objectMapper.writeValueAsString(recommendAddUserTemplateRequest);
+        String response = objectMapper.writeValueAsString(RestResponse.ok());
+
+        mockMvc.perform(post("/api/recommend")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request)
+                        .header(HttpHeader.USER_TOKEN, userToken))
+                .andExpect(status().isOk())
+                .andExpect(content().string(response))
+                .andDo(document("recommend-add-user-template-add-items",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestHeaders(
+                                headerWithName("ahmatda-user-token").description("유저 UUID")
+                        )))
+                .andDo(print());
+    }
+    @Test
+    void 추천템플릿_기존_카테고리에_새로운_템플릿_저장() throws Exception {
+        User user = User.createUserWithUserToken("FCDBD8EF-62FC-4ECB-B2F5-92C9E79AC7F0");
+        String userToken = user.getUserToken();
+        Long userCategoryId = 1L;
+        Long userTemplateId = 10L;
+        List<String> items = Lists.newArrayList("여권", "증명사진", "노트북");
+
+        // 기존카테고리 + 새로운 템플릿에 추가
+        RecommendCreateTemplateRequest recommendCreateTemplateRequest = RecommendCreateTemplateRequest.builder()
+                .templateName("추가할 템플릿이름")
+                .userCategoryId(userCategoryId)
+                .items(items)
+                .build();
+
+        RecommendAddUserTemplateRequest recommendAddUserTemplateRequest = RecommendAddUserTemplateRequest.builder()
+                .createTemplateRequest(recommendCreateTemplateRequest)
+                .build();
+
+        String request = objectMapper.writeValueAsString(recommendAddUserTemplateRequest);
+        String response = objectMapper.writeValueAsString(RestResponse.ok());
+
+        mockMvc.perform(post("/api/recommend")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request)
+                        .header(HttpHeader.USER_TOKEN, userToken))
+                .andExpect(status().isOk())
+                .andExpect(content().string(response))
+                .andDo(document("recommend-create-template",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestHeaders(
+                                headerWithName("ahmatda-user-token").description("유저 UUID")
+                        )))
+                .andDo(print());
+    }
+
 }
