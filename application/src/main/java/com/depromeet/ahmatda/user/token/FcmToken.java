d33ebc651cac93f19821853c68a1f22ce4ee9150
suchan <aud248@naver.com>
2022-12-21 17:20:01 +0900

    :white_check_mark: REST DOCS 명세

--- application/src/main/java/com/depromeet/ahmatda/user/token/FcmToken.java
+++ application/src/main/java/com/depromeet/ahmatda/user/token/FcmToken.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class FcmToken {
 
+    @NotBlank(message = "FCM TOKEN은 필수값입니다.")
     private String fcmToken;
-
 
 }
