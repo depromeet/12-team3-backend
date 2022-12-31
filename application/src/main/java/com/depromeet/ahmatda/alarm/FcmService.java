7a3ef989ae3b1599830b76cfc5046776989e1f4f
suchanmyoung <aud248@naver.com>
2022-12-31 18:13:46 +0900

    로그용 로직 추가

--- application/src/main/java/com/depromeet/ahmatda/alarm/FcmService.java
+++ application/src/main/java/com/depromeet/ahmatda/alarm/FcmService.java
@@ -27,39 +27,44 @@ public class FcmService {
 
     private String makeMessage(final String token) throws JsonProcessingException {
         final FcmMessage.Notification notification = FcmMessage.Notification.builder()
-                .title("TestTitle")
-                .body("TestBody")
-                .build();
+            .title("TestTitle")
+            .body("TestBody")
+            .build();
 
         final FcmMessage fcmMessage = FcmMessage.builder()
-                .message(FcmMessage.Message.builder()
-                        .token(token)
-                        .notification(notification)
-                        .build())
-                .build();
+            .message(FcmMessage.Message.builder()
+                .token(token)
+                .notification(notification)
+                .build())
+            .build();
         return objectMapper.writeValueAsString(fcmMessage);
     }
 
     private String getAccessToken() throws IOException {
         final String fcmAccountPath = "firebase/fcm-account.json";
-
-        final GoogleCredentials googleCredentials = GoogleCredentials
-            .fromStream(new ClassPathResource(fcmAccountPath).getInputStream())
-            .createScoped(Arrays.asList("https://www.googleapis.com/auth/cloud-platform"));
-        googleCredentials.refreshIfExpired();
-        return googleCredentials.getAccessToken().getTokenValue();
+        try {
+            final GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource(fcmAccountPath).getInputStream())
+                .createScoped(Arrays.asList("https://www.googleapis.com/auth/cloud-platform"));
+            googleCredentials.refreshIfExpired();
+            return googleCredentials.getAccessToken().getTokenValue();
+        } catch (Exception e) {
+            log.error(String.valueOf(e));
+        }
+        return null;
     }
 
     private void send(final String message) throws IOException {
         final OkHttpClient okHttpClient = new OkHttpClient();
-        final RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+        final RequestBody requestBody = RequestBody.create(message,
+            MediaType.get("application/json; charset=utf-8"));
 
         final Request request = new Request.Builder()
-                .url("https://fcm.googleapis.com/v1/projects/ahmatda-bb816/messages:send")
-                .post(requestBody)
-                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
-                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
-                .build();
+            .url("https://fcm.googleapis.com/v1/projects/ahmatda-bb816/messages:send")
+            .post(requestBody)
+            .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+            .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+            .build();
 
         final Response response = okHttpClient.newCall(request).execute();
         log.info("Response = {}", response.body().string());
