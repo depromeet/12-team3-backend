46ec81d6b553263c2f5226c7efaf9be21e978990
자신의 닉네임 <cmg1411@naver.com>
2022-12-31 17:42:11 +0900

    :fire: 회원가입 validation 누락 수정

--- application/src/main/java/com/depromeet/ahmatda/user/dto/SignUpRequest.java
+++ application/src/main/java/com/depromeet/ahmatda/user/dto/SignUpRequest.java
@@ -6,11 +6,14 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+
 @Getter
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpRequest {
 
+    @Valid
     private OnboardingRequest onboardingRequest;
 }
