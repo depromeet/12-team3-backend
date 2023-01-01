8e1e804abe3ac7761e4200cc57adced9b8c020fa
suchanmyoung <aud248@naver.com>
2023-01-01 19:36:26 +0900

    :poop: remove user method

--- application/src/main/java/com/depromeet/ahmatda/user/service/UserService.java
+++ application/src/main/java/com/depromeet/ahmatda/user/service/UserService.java
@@ -3,15 +3,12 @@ package com.depromeet.ahmatda.user.service;
 import com.depromeet.ahmatda.domain.user.User;
 import com.depromeet.ahmatda.user.UserRegisterCode;
 import com.depromeet.ahmatda.user.dto.SignUpRequest;
-import com.depromeet.ahmatda.alarm.dto.FcmTokenRequest;
 import com.depromeet.ahmatda.user.token.UserToken;
-
 import java.util.Optional;
 
 public interface UserService {
 
     UserRegisterCode checkUser(UserToken userToken);
     UserToken createUser(SignUpRequest request);
-    void renewFcmToken(final String userToken, final FcmTokenRequest token);
     Optional<User> getUserByToken(String userToken);
 }
