60baf4d519dc651965d696ba3f349341b32669b3
자신의 닉네임 <cmg1411@naver.com>
2023-01-09 23:39:05 +0900

    홈 화면 추천아이템 응답 수정

---
+++ application/src/main/java/com/depromeet/ahmatda/recommend/dto/SectionRecommendItemData.java
@@ -0,0 +1,11 @@
+package com.depromeet.ahmatda.recommend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SectionRecommendItemData {
+    Long recommendItemId;
+    String name;
+}
---
+++ application/src/main/java/com/depromeet/ahmatda/recommend/dto/SectionRecommendItemResponse.java
@@ -0,0 +1,16 @@
+package com.depromeet.ahmatda.recommend.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class SectionRecommendItemResponse {
+    private String comment;
+    private List<SectionRecommendItemData> items;
+}
